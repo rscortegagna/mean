d784e3174384ae576ec42d70375ecc1b7fd2af6e
Rafael Scortegagna <rscortegagna@gmail.com>
2021-08-14 09:11:34 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+//https://adp-gptlearning.udemy.com/course/angular-2-and-nodejs-the-practical-guide/learn/lecture/10416234#overview
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+import { AppComponent } from './app.component';
+import { PostCreateComponent} from './posts/post-create/post-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
